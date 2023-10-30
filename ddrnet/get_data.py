c5f82905301ccec576f02e9d83798267d7d194f3
Nanos Giorgos <74103191+Nanos1@users.noreply.github.com>
2023-10-30 09:52:49 +0200

    Add files via upload

---
+++ ddrnet/get_data.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+IMAGE_SIZE = 256
+#BATCH_SIZE = 2 
+def read_image(image_path, mask=False):
+    image = tf.io.read_file(image_path)
+    if mask:
+        image = tf.image.decode_png(image, channels=1)
+        image.set_shape([None, None, 1])
+        image = tf.image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
+        image = image / 255
+    else:
+        image = tf.image.decode_png(image, channels=3)
+        image.set_shape([None, None, 3])
+        image = tf.image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
+        image = image / 255
+    return image
+
+
+def load_data(image_list, mask_list):
+    image = read_image(image_list)
+    mask = read_image(mask_list, mask=True)
+    return image, mask
+
+
+def data_generator(image_list, mask_list, batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices((image_list, mask_list))
+    dataset = dataset.map(load_data)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    return dataset
+
+
+
